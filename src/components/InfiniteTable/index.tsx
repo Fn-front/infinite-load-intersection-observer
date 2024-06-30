7eda69368cea8c7c5862c7fcacbe6137339080da
福見 直人 <naoto.fukumi@arsaga.jp>
2024-06-30 19:44:48 +0900

    hooks コンポーネント実装

--- src/components/InfiniteTable/index.tsx
+++ src/components/InfiniteTable/index.tsx
@@ -1,41 +1,79 @@
 'use client'
 
-import { useState, useEffect } from 'react'
+import { useIntersectionObserver } from '@/hooks/IntersectionObserver';
+import { useState, useEffect, useRef } from 'react'
 
 export const InfiniteTable = () => {
 
   const [data, setData] = useState<string[]>([]);
+  const [refData, setRefData] = useState<string[]>([])
+  const scrollArea = useRef(null)
+  const targetElement = useRef<HTMLTableRowElement>(null!)
+
+  const intersectionObserverOptions = {
+    root: scrollArea.current,
+    rootMargin: '0px',
+    threshold: 1.0
+  }
+
+  const handleIntersectionObserver = (entries: IntersectionObserverEntry[]) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        // IntersectionObserverで条件を満たした時に実行
+        // 連番の配列
+        const array: Array<string> = [...Array(100)].map((_, i) => 'item'+((data.length + (i+1))))
+        // 配列格納
+        setData([...data, ...array])
+        setRefData([...refData, entry.target.rowIndex]);
+        
+      }
+    })
+  }
+
+  // IntersectionObserver実行
+  useIntersectionObserver([targetElement], handleIntersectionObserver, intersectionObserverOptions)
 
   useEffect(() => {
-
+    
     // 初期データ作成
     // state変更とrender更新で無限呼び出しになるのでuseEffect内に記載
     const array: Array<string> = [...Array(100)].map((_, i) => 'item'+(i+1))
+    // 配列格納
     setData(array)
 
   }, [])
 
   return (
-    <div style={{ overflowY: 'scroll', width: '70vw', margin: '32px auto 0' }}>
-      <div style={{ maxHeight: '500px' }}>
-        <table style={{ width: '100%' }}>
-          <thead>
-            <tr>
-              <th style={{ padding: '8px', width: '200px' }}>No</th>
-              <th style={{ padding: '8px' }}>item</th>
-            </tr>
-          </thead>
-          <tbody>
-          { data.map((item, index) => {
-            return (
-              <tr key={index}>
-                <td style={{ padding: '4px 8px' }}>No. {index+1}</td>
-                <td style={{ padding: '4px 8px' }}>{ item }</td>
+    <div>
+      <div style={{ overflowY: 'scroll', width: '70vw', margin: '32px auto 0' }} ref={scrollArea}>
+        <div style={{ maxHeight: '500px' }}>
+          <table style={{ width: '100%' }}>
+            <thead>
+              <tr>
+                <th style={{ padding: '8px', width: '200px' }}>No</th>
+                <th style={{ padding: '8px' }}>item</th>
               </tr>
+            </thead>
+            <tbody>
+            { data.map((item, index) => {
+              return (
+                <tr key={index+1} ref={ data.length - 50 == index+1 ? targetElement : null }>
+                  <td style={{ padding: '4px 8px' }}>No. {index+1}</td>
+                  <td style={{ padding: '4px 8px' }}>{ item }</td>
+                </tr>
+              )
+            }) }
+            </tbody>
+          </table>
+        </div>
+      </div>
+      <div style={{ marginTop: '32px' }}>
+        <p>通過した連番</p>
+        { refData.map((item, index) => {
+            return (
+              <p key={index}>{ item }</p>
             )
           }) }
-          </tbody>
-        </table>
       </div>
     </div>
   )
